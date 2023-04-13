89d87a40675d2fd775e08adab91e443c6d4d05d1
NoelOsiro <noel.osiro@azubiafrica.org>
2023-04-13 03:40:32 +0200

    Finished core values

--- src/Components/Features/Feature.tsx
+++ src/Components/Features/Feature.tsx
@@ -1,129 +1,14 @@
 import React from 'react';
-import './feat.css';
-import { Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle, Col, Container, Row, } from 'reactstrap'
-import { features } from './fetaures';
 
+import Services from './Services';
+import Solutions from './Solutions';
 const Feature = () => {
     return (
         <>
-            <section className="features-tiles section mt-5">
-                <Container>
-                    <div className="features-tiles-inner section-inner pt-0">
-                        <div className="section-header center-content">
-                            <div className="container-xs">
-                                <h2 className="mt-0 mb-4 display-4">Build up the whole picture</h2>
-                                <h3 className="m-0 text-muted mb-5">
-                                    Excepteur sint occaecat cupidatat non proident,
-                                    sunt in culpa qui officia deserunt mollit anim id est laborum —
-                                    semper quis lectus nulla at volutpat diam ut venenatis.
-                                </h3>
-                            </div>
-                        </div>
-                        <Row>
-                            {features.map((item) => (
-                                <Col xs={4} key={item.title}>
-                                    <Card className='m-3'>
-                                        <CardImg
-                                            alt="Card image cap"
-                                            src={item.image} top
-                                            width="100%"
-                                        />
-                                        <CardBody>
-                                            <CardTitle tag="h5">
-                                                <u>{item.title}</u>
-                                            </CardTitle>
-                                            <CardSubtitle className="mb-2 text-muted" tag="h6">
-                                                Sex Education
-                                            </CardSubtitle>
-                                            <CardText>
-                                                {item.text}
-                                            </CardText>
-                                        </CardBody>
-                                    </Card>
-                                </Col>
-                            ))}
-                        </Row>
-                    </div>
-                </Container>
-            </section>
-            <section id="services" className="services blue-bg">
-                <div className="container text-center">
-                    <h1 className="small-title" style={{ margin: '70px 0', color: '#fff !important' }}>What service are you interested in?</h1>
-                    <div className="">
-                        <div className="row visible-xs">
-                            <div className="container">
-                                <div className="col-xs-10 col-xs-offset-1">
-                                    <a href="branding" className="service_item">
-                                        <div className="whiteBtns">
-                                            <p><span className="">Branding &amp; Logo design</span><span></span><span className="pull-right"><i className="fas fa-chevron-right"></i></span></p>
-                                        </div>
-                                    </a>
-                                    <a href="webapp" className="service_item">
-                                        <div className="whiteBtns">
-                                            <span className="">Web Development</span><span></span><span className="pull-right"><i className="fas fa-chevron-right"></i></span>
-                                        </div>
-                                    </a>
-                                    <a href="android" className="service_item"><div className="whiteBtns">
-                                        <span className="">Mobile App Development</span><span></span><span className="pull-right"><i className="fas fa-chevron-right"></i></span>
-                                    </div></a>
-                                    <a href="seo" className="service_item"><div className="whiteBtns">
-                                        <span className="">SEO</span><span></span><span className="pull-right"><i className="fas fa-chevron-right"></i></span>
-                                    </div></a>
-                                    <a href="socialMediaMarketing" className="service_item"><div className="whiteBtns">
-                                        <span className="">Social Media Marketing</span><span></span><span className="pull-right"><i className="fas fa-chevron-right"></i></span>
-                                    </div></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row hidden-xs">
-                            <div className="col-sm-4 ">
-                                <div className="service_link">
-                                    <div className="services-image">
-                                        <img alt="Web Development" src="img/web.svg" className="services-img" />
-                                    </div>
-
-                                    <div className="service-title">Web Development</div>
-                                    <div className="service_links">
-                                        <a href="static_site" className="service_item">Static Sites</a>
-                                        <a href="ecommerce" className="service_item">Ecommerce</a>
-                                        <a href="webapp" className="service_item">Web Applications</a>
-                                        <a href="seo" className="service_item">SEO</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-4">
-                                <div className="service_link">
-                                    <div className="services-image text-center">
-                                        <img alt="Mobile Development" src="img/mobile.svg" className="services-img" />
-                                    </div>
-                                    <div className="service-title">Mobile Development</div>
-                                    <div className="service_links">
-                                        <a href="android" className="service_item">Android</a>
-                                        <a href="ios" className="service_item">iOS</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-4">
-                                <div className="service_link">
-                                    <div className="services-image">
-                                        <img alt="Web Development" src="img/magic-wand.svg" className="services-img" />
-                                    </div>
-                                    <div className="service-title">Other Services</div>
-                                    <div className="service_links">
-                                        <a href="payment" className="service_item">Payment Integrations</a>
-                                        <a href="consultancy" className="service_item">Software Consultancy</a>
-                                        <a href="socialMediaMarketing" className="service_item">Social Media Marketing</a>
-                                        <a href="branding" className="service_item">Branding &amp; Logo design</a>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
-            </section>
+        <Services/>
+        <Solutions/>
         </>
+            
     )
 }
 
---
+++ src/Components/Features/Solutions.tsx
@@ -0,0 +1,200 @@
+import React from 'react';
+import './sols.css';
+
+const Solutions = () => {
+  return (
+    <section id="solutions" className="solutions gray-bg" style={{paddingBottom: "0px"}}>
+        	<div className="container">
+        		<h1 className="small-title main-title solutions-title text-center">Some solutions</h1>
+	        	<div>
+	        		<div className="row">
+	        			<div className="carousel slide" data-ride="carousel" id="quote-carousel" style={{height: "auto "}}>		
+					        <div className="carousel-inner">
+					        <div className="item">
+					          	<div className="col-md-6">
+			        				 <img src="img/works/duty.png" style={{objectFit: "cover",maxWidth: "100%"}}/> 
+			        			</div>
+			        			<div className="col-md-6">
+			        				<div className="solution_title">Building an import duty Calculating platform</div>
+			        				<div className="solution_text">
+			        					<p>Dutycalc is an online data-based tool developed with the main intention of enabling Kenyan used motor vehicle importers to calculate Custom duty and other surcharges.</p>
+			        					<p style={{color: "#1E77A7"}}>
+			        						<a href="https://dutycalc.co.ke/" target="_blank">See platform</a>
+			        					</p>
+			        				</div>
+			        				<div className="solution_stats row hidden-xs">
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">0.65+m</p>
+			        						<p className="stats_text">Monthly users</p>
+			        					</div>
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">5k+</p>
+			        						<p className="stats_text">Daily calculations</p>
+			        					</div>
+			        				</div>
+			        			</div>
+					        </div>
+	
+					          <div className="item">
+					          	<div className="col-md-6 col-xs-12 text-center">
+			        				<img src="img/works/uamuzi.png" className="phoneImg" style={{objectFit: "cover",maxWidth: "100%", maxHeight: "410px"}}/>
+			        			</div>
+			        			<div className="col-md-6 col-xs-12">
+			        				<div className="solution_title">Building an android based phone application</div>
+			        				<div className="solution_text">
+			        					<p>Uamuzi is a mobile based tool developed with the main intention of enabling Kenyans to communicate freely with their leaders in their respective administrative units.</p>
+			        					<p style={{color: "#1E77A7"}}>
+			        						<a href="https://play.google.com/store/apps/details?id=ke.co.techmata.uamuzi&amp;hl=en" target="_blank">See platform</a>
+			        					</p>
+			        				</div>
+			        				<div className="solution_stats row hidden-xs">
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">3k+</p>
+			        						<p className="stats_text">Daily chats</p>
+			        					</div>
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">200+</p>
+			        						<p className="stats_text">Downloads</p>
+			        					</div>
+			        				</div>
+			        			</div>
+					            
+					          </div>
+
+					          <div className="item active">
+					          	<div className="col-md-6 col-xs-12 text-center">
+			        				<img src="img/works/quotr.png" style={{objectFit: "cover",maxWidth: "100%", maxHeight: "410px"}}/>
+			        			</div>
+			        			<div className="col-md-6 col-xs-12">
+			        				<div className="solution_title">A business aid application</div>
+			        				<div className="solution_text">
+			        					<p>Quotr,helps you create, amend and send quotations instantly. Quotr enables you to marketing enabling individuals to filter through all the clutter and find exactly what it is that they’re looking for.</p>
+			        					<p style={{color: "#1E77A7"}}>
+			        						<a href="https://play.google.com/store/apps/details?id=quotr.info.quotr" target="_blank">See platform</a>
+			        					</p>
+			        				</div>
+			        				<div className="solution_stats row hidden-xs">
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">65+</p>
+			        						<p className="stats_text">Weekly sent qoutes</p>
+			        					</div>
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">170+</p>
+			        						<p className="stats_text">Registered Businesses</p>
+			        					</div>
+			        				</div>
+			        			</div>
+					          </div>
+                                <div className="item">
+					          	<div className="col-md-6 col-xs-12">
+			        				<img src="img/works/CASE.png" style={{objectFit: "cover",maxWidth: "100%", maxHeight: "410px"}}/>
+			        			</div>
+			        			<div className="col-md-6 col-xs-12" >
+			        				<div className="solution_title">Building an e-Commerce platform for top Fashion line in Nairobi</div>
+			        				<div className="solution_text">
+			        					<p>Airi is one of the top upcoming fashion lines in Kenya, fixated on unique handiwork and superlative fabrics to Empowering women through clothing Made in Kenya.</p>
+			        					<p style={{color: "#1E77A7"}}>
+			        						<a href="http://airionline.com/" target="_blank">See platform</a>
+			        					</p>
+			        				</div>
+			        				<div className="solution_stats row hidden-xs">
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">100+</p>
+			        						<p className="stats_text">Monthly users</p>
+			        					</div>
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">5k+</p>
+			        						<p className="stats_text">Daily users</p>
+			        					</div>
+			        				</div>
+			        			</div>
+					          </div> 
+					          <div className="item">
+					          	<div className="col-md-6 col-xs-12 text-center">
+			        				<img src="img/works/beba.png" style={{objectFit: "cover",maxWidth: "100%", maxHeight: "410px"}}/>
+			        			</div>
+			        			<div className="col-md-6 col-xs-12">
+			        				<div className="solution_title">Building a transit android mobile application</div>
+			        				<div className="solution_text">
+			        					<p>Beba Networks, provide commuting and percel delivery services using motorcycles and tuktuks.</p>
+			        					<p style={{color: "#1E77A7"}}>
+			        						<a href="https://play.google.com/store/apps/details?id=com.techmata.beba.app&amp;hl=en" target="_blank">See platform</a>
+			        					</p>
+			        				</div>
+			        				<div className="solution_stats row hidden-xs">
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">100+</p>
+			        						<p className="stats_text">Monthly users</p> 
+			        					</div>
+			        					<div className="col-sm-6 stats_item">
+			        						 <p className="stats_value">500+</p>
+			        						<p className="stats_text">Monthly users</p> 
+			        					</div>
+			        				</div>
+			        			</div>
+					              
+					          </div>
+
+					          <div className="item">
+					          	<div className="col-md-6 col-xs-12 text-center">
+			        				<img src="img/works/superior.png" style={{objectFit: "cover",maxWidth: "100%", maxHeight: "410px"}}/> 
+			        			</div>
+			        			<div className="col-md-6 col-xs-12">
+			        				<div className="solution_title">A retail selling android mobile application</div>
+			        				<div className="solution_text">
+			        					<p>ElimuAdvisor seeks to advice potential students about admission criteria into prospective institutions of higher learning in Kenya.</p>
+			        					<p style={{color: "#1E77A7"}}>
+			        						<a href="http://elimuadvisor.co.ke/" target="_blank">See platform</a>
+			        					</p>
+			        				</div>
+			        				<div className="solution_stats row hidden-xs">
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">50+</p>
+			        						<p className="stats_text">Daily Active Sellers</p>
+			        					</div>
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">5+</p>
+			        						<p className="stats_text">Retail Stores </p>
+			        					</div>
+			        				</div>
+			        			</div>
+					          </div>
+	
+					          <div className="item">
+					          	<div className="col-md-6 col-xs-12">
+			        				 <img src="img/works/elimu.png" style={{objectFit: "cover", maxWidth: "100%", maxHeight: "410px;"}}/> 
+			        			</div>
+			        			<div className="col-md-6 col-xs-12">
+			        				<div className="solution_title">An educational aid web application</div>
+			        				<div className="solution_text">
+			        					<p>ElimuAdvisor seeks to advice potential students about admission criteria into prospective institutions of higher learning in Kenya.</p>
+			        					<p style={{color: "#1E77A7"}}>
+			        						<a href="http://elimuadvisor.co.ke/" target="_blank">See platform</a>
+			        					</p>
+			        				</div>
+			        				<div className="solution_stats row hidden-xs">
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">3K+</p>
+			        						<p className="stats_text">Monthly searches</p>
+			        					</div>
+			        					<div className="col-sm-6 stats_item">
+			        						<p className="stats_value">5k+</p>
+			        						<p className="stats_text">Registered Users</p>
+			        					</div>
+			        				</div>
+			        			</div>
+					          </div>
+					        </div>					        
+					        <a data-bs-slide="prev" href="#quote-carousel" className="left carousel-control hidden-xs" style={{height: "5px;"}}><svg className="slick-arrow slick-prev" width="40" height="35" viewBox="0 0 30 22" style={{transform: "rotate(180deg)", zIndex: "1000;"}}><g stroke-width="2" fill="none" fill-rule="evenodd"><path d="M0 11h28"></path><path d="M19 2l9 9-9 9"></path></g></svg></a>
+					        <a data-bs-slide="next" href="#quote-carousel" className="right carousel-control hidden-xs" style={{height: "5px;"}}><svg className="slick-next" width="40" height="35" viewBox="0 0 30 22" style={{zIndex: "1000"}}><g stroke-width="2" fill="none" fill-rule="evenodd"><path d="M0 11h28"></path><path d="M19 2l9 9-9 9"></path></g></svg></a>
+					      </div> 
+	        		</div>
+	        	</div>
+        	</div>
+
+        	
+        </section>
+  )
+}
+
+export default Solutions
