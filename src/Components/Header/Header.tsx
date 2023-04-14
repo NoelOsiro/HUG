40d2abe9f1a5c16a9f10ddfa0304e56787bd1546
NoelOsiro <noel.osiro@azubiafrica.org>
2023-04-14 04:15:23 +0200

    Edited homepage

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,21 +5,21 @@ import './header.css';
 const Header = () => {
   return (
     <section className="site-header center-content mb-4 ">
-      <div className="container-sm">
+      <div className="container-fluid vid">
         <div className="hero-inner section-inner">
           <div className="hero-content">
-            <h1 className="mt-0 mb-5 display-4">We serve our <span className="text-primary">generation</span>
+            <div className="video-wrapper">
+            <video width="100%" height="504" playsInline autoPlay muted loop>
+              <source src={headervid} type="video/mp4" />
+            </video>  
+          </div>
+          <h1 className="mt-4 mb-5 display-4">We serve our <span className="text-primary">generation</span>
             </h1>
             <div className="container-xs">
               <h3 className="m-0 mb-5 text-muted">
                 Meaningful youth’s engagement, non-discrimination, accountability and integrity.
               </h3>
             </div>
-          </div>
-          <div className="video-wrapper">
-            <video width="896" height="504" playsInline autoPlay muted loop>
-              <source src={headervid} type="video/mp4" />
-            </video>
           </div>
         </div>
       </div>
